90fc83b935664644bc61209e4101722d1ca20809
Emmanuel Huitrado <emmanuel.huitrado@unosquare.com>
2021-07-09 20:03:55 -0500

    Adding relationships

---
+++ src/main/java/sfg/tutorial/sfgrecipes/SpringSFGRecipesApplication.java
@@ -0,0 +1,11 @@
+package sfg.tutorial.sfgrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSFGRecipesApplication {
+    public static void main(String ...args){
+        SpringApplication.run(SpringSFGRecipesApplication.class, args);
+    }
+}
